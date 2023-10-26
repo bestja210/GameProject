a49ecdd050a83e543bbb6fa8a4a0468352bf6290
Jacob Best <bestja210@gmail.com>
2023-10-26 19:21:41 -0400

    First Commit

---
+++ GameProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameProject
+//
+//  Created by Jacob Best on 10/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
